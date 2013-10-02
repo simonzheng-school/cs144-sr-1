e44a39507c4066fd04b423f5f84c59778f667f91
Simon Zheng <simonzheng@stanford.edu>
2013-10-02 02:59:10 -0700

    lol

--- router/sr_router.c
+++ router/sr_router.c
@@ -95,7 +95,7 @@ void sr_handlepacket(struct sr_instance* sr,
 		if (sr_if_list_contains_ip(sr, iphdr->ip_dst)) { /* If the packet is for the router */
       printf ("The IP packet is for me!\n");
       sr_icmp_hdr_t *icmp_hdr = (sr_icmp_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t)); 
-      if (icmp_hdr->icmp_type == 8 && icmp_hdr->type == 0) { /* If this is an echo request */
+      if (icmp_hdr->icmp_type == 8 && icmp_hdr->icmp_code == 0) { /* If this is an echo request */
         /* If it's ICMP echo req, send echo reply. */
         /* fill in code here */
       } 
