8e2615e685b3281b761533cbcc11e7e6daa658f7
Simon Zheng <simonzheng@stanford.edu>
2013-10-02 11:18:32 -0700

    lol

--- router/sr_router.c
+++ router/sr_router.c
@@ -191,8 +191,8 @@ void sr_routing_table_lpm_forwarding(struct sr_instance* sr, uint32_t ip_addr)
   /* Traverse the routing table searching for the gateway address with the greatest match */
   rt_walker = sr->routing_table;
 
-  uint32_t next_gw = rt_walker->gw->s_addr;
-  uint32_t longest_mask = rt_walker->mask->s_addr;
+  uint32_t next_gw = rt_walker->gw.s_addr;
+  uint32_t longest_mask = rt_walker->mask.s_addr;
 
   printf("LPM: The first routing entry is as follows and has mask == %x: \n\t", rt_walker->mask.s_addr);
   sr_print_routing_entry(rt_walker);
@@ -226,8 +226,11 @@ void sr_routing_table_lpm_forwarding(struct sr_instance* sr, uint32_t ip_addr)
       if (masked_ip == rt_walker->dest.s_addr) {
         printf("\twe have a match with \n");
         sr_print_routing_entry(rt_walker);
-        next_gw = rt_walker->gw.s_addr;
-        longest_mask = rt_walker->gw.s_addr;
+        if (rt_walker->gw.s_addr > longest_mask) {
+          printf("\t *** we have a new longest match! *** \n");
+          next_gw = rt_walker->gw.s_addr;
+          longest_mask = rt_walker->gw.s_addr;
+        }
       }
       /* e.g.: uint32_t gateway_addr = calculate_prefix_match(rt_walker, max_match); */
   }
